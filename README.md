9e658c00f7a76bffee53c06283d3755638d7cda7
U-madpc\madlex <madlex@madpc.(none)>
2015-05-04 14:31:02 +0300

    updated readme file, adding script

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
-# plgcalc
 Python planetary gear calculator
+----------
+DESCRIPTION
+----------
+
+Planetary gears must respect certain mathematical equations in order to work properly. One basic rule is that the number of teeth the ring gear must be equal to twice the number of teeth the planet gear have plus the number of teeth the sun gear has. 
+
+  R = 2 * P + S
+
+Another is that for a planetary gear ccnfiguration with an even distribution of planets (that is, planets are evenly spaced), the number of teeth of ring gear and sun gear must evenly divisible by the number of planets.
+
+  R divides #Planets AND S divides #Planets
+
+This calculator helps generating these possible gear configurations while calculating turn ratios in different scenarios: when ring gear is fixed, when carrier (planets  position) is fixed or when sun is fixed.
+
+----------
+EXAMPLES
+----------
+1. Check if the following configuration is correct:
+	ring gear   = 36 teeth
+	planet gear = 14 teeth
+	sun gear    = 8 teeth
+
+> plgcalc.py 36:14:8
+36:14:08
+
+1. Check if the following configuration is correct:
+	ring gear   = 36 teeth
+	planet gear = 14 teeth
+	sun gear    = 7 teeth
+
+> plgcalc.py 36:14:7
+No results possible: invalid gear configuration
+
+3. Print out all data regarding the first configuration:
+
+> plgcalc.py 36:14:8 --all
+Gear            Tp/Ts Ratio     Tp/Tr Ratio     Ts/Tr Ratio     Angle
+Configuration   Ring fixed      Sun fixed       Planets fixed   Planet-Sun
+36:14:08        0.181818        0.818182        -4.500000       8.181818
+
+4. Check the configuration that is possible for a planetary having:
+	ring gear   = 42 teeth
+	planet gear = 18 teeth
+	sun gear    = any
+
+   Note: for this configuration print Planet-Ring turn ratio (sun is fixed)
+	
+> plgcalc.py 42:18: --tpr
+42:18:06        0.875000
+
+5. Print 10 possible configurations for a planetary setup that has:
+	sun gear    = 12 teeth
+	
+   Note: for the resulting configurations print Planets-Sun turn ratio (ring is fixed)
+
+> plgcalc.py ::12 -l 10 --tps --header
+Gear            Tp/Ts Ratio
+Configuration   Ring fixed
+14:01:12        0.461538
+16:02:12        0.428571
+18:03:12        0.400000
+20:04:12        0.375000
+22:05:12        0.352941
+24:06:12        0.333333
+26:07:12        0.315789
+28:08:12        0.300000
+30:09:12        0.285714
+32:10:12        0.272727
+
+6. Generate all possible configurations for a planetary gear set with following params:
+	sun gear    = 9 teeth
+	planets     = 3 (evenly distributed)
+
+> plgcalc.py ::9 -l 10 -e -p 3
+15:03:09
+21:06:09
+27:09:09
+33:12:09
+39:15:09
+45:18:09
+51:21:09
+57:24:09
+63:27:09
+69:30:09
+
+7. Same as above, but also print calculated ratios:
+
+> plgcalc.py ::9 -l 10 -e -p 3 --all
+Gear            Tp/Ts Ratio     Tp/Tr Ratio     Ts/Tr Ratio     Angle
+Configuration   Ring fixed      Sun fixed       Planets fixed   Planet-Sun
+15:03:09        0.375000        0.625000        -1.666667       15.000000
+21:06:09        0.300000        0.700000        -2.333333       12.000000
+27:09:09        0.250000        0.750000        -3.000000       10.000000
+33:12:09        0.214286        0.785714        -3.666667       8.571429
+39:15:09        0.187500        0.812500        -4.333333       7.500000
+45:18:09        0.166667        0.833333        -5.000000       6.666667
+51:21:09        0.150000        0.850000        -5.666667       6.000000
+57:24:09        0.136364        0.863636        -6.333333       5.454545
+63:27:09        0.125000        0.875000        -7.000000       5.000000
+69:30:09        0.115385        0.884615        -7.666667       4.615385
+
+8. Same as above, but use sun gear with 8 teeth, which  is an invalid configuration:
+
+> plgcalc.py ::8 -l 10 -e -p 3 --all
+No results possible: invalid sun configuration, planets will never be evenly spaced
+
---
+++ plgcalc.py
@@ -0,0 +1,414 @@
+#!/usr/local/bin/python
+
+import sys
+import argparse
+from os import path
+
+# globals
+parser = argparse.ArgumentParser(description='Planetary gear calculator')
+args = None
+
+# command line options
+class OPTIONS:
+    EVEN_SPACING="e"
+    PRINT_HEADER="nh"
+    PRINT_ALL="so"
+    PRINT_TPS = "tps"
+    PRINT_TPR = "tpr"
+    PRINT_TSR = "tsr"
+    PRINT_ANG = "ang"
+
+# program configuration
+class CONFIG:
+    ring   = 0
+    planet = 0
+    sun    = 0
+    print_header = False
+    print_tps    = False
+    print_tpr    = False
+    print_tsr    = False
+    print_angle  = False
+
+    # transform command line options into program configuration
+    @staticmethod
+    def from_options(options):
+
+        # print only what is requested
+        if OPTIONS.PRINT_ALL in options:
+            CONFIG.print_header = True
+            CONFIG.print_angle = True
+            CONFIG.print_tpr = True
+            CONFIG.print_tps = True
+            CONFIG.print_tsr = True
+            CONFIG.print_ang = True
+        else:
+            if OPTIONS.PRINT_TPR in options:
+                CONFIG.print_tpr = True
+            if OPTIONS.PRINT_TPS in options:
+                CONFIG.print_tps = True
+            if OPTIONS.PRINT_TSR in options:
+                CONFIG.print_tsr = True
+            if OPTIONS.PRINT_ANG in options:
+                CONFIG.print_angle = True
+
+        # no header option
+        if OPTIONS.PRINT_HEADER in options:
+            CONFIG.print_header = True
+
+        # gear configuration
+        try:
+            R,P,S = str(args.gear).split(':',3)
+        except:
+            warning("Invalid arguments format")
+            usage()
+            exit()
+
+        CONFIG.ring   = convert_to_int(R)
+        CONFIG.planet = convert_to_int(P)
+        CONFIG.sun    = convert_to_int(S)
+
+#
+# helper functions
+#
+
+def convert_to_int(string):
+    try: return int(string)
+    except: return 0
+
+def warning(warn):
+    #sys.stderr.write("Warning: %s\n" % warn)
+    print warn
+
+def valid_planetary(ring, planet, sun, even_planets):
+    # check if R = 2 * P + S
+    if ring != 2 * planet + sun:
+        return False
+
+    if even_planets is not None:
+        if ring % even_planets != 0 or sun % even_planets != 0:
+            return False # planets ar not even
+
+    return True
+
+#
+# print functions
+#
+
+def print_header():
+
+    if CONFIG.print_header == False:
+        return
+
+    prs1 = "Gear\t"
+    prs2 = "Configuration"
+
+    if CONFIG.print_tps:
+        prs1 = prs1 + "\tTp/Ts Ratio"
+        prs2 = prs2 + "\tRing fixed"
+
+    if CONFIG.print_tpr:
+        prs1 = prs1 + "\tTp/Tr Ratio"
+        prs2 = prs2 + "\tSun fixed"
+
+    if CONFIG.print_tsr:
+        prs1 = prs1 + "\tTs/Tr Ratio"
+        prs2 = prs2 + "\tPlanets fixed"
+
+    if CONFIG.print_angle:
+        prs1 = prs1 + "\tAngle"
+        prs2 = prs2 + "\tPlanet-Sun"
+
+    print prs1
+    print prs2
+
+def print_entry(ring,planet,sun):
+
+    # Ratio formula:
+    # ( R + S ) * Tp = R * Tr + S * Ts
+    # => Ring    fixed : Tp/Ts = S / (R+S)
+    # => Sun     fixed : Tp/Tr = R / (R+S)
+    # => Planets fixed : Ts/Tr = - R / S
+
+    prs = "%.2u:%.2u:%.2u" % (ring,planet,sun)
+
+    if CONFIG.print_tps:
+        prs = prs + "\t%f" % (sun / float(sun+ring))
+
+    if CONFIG.print_tpr:
+        prs = prs + "\t%f" % (ring / float(ring+sun))
+
+    if CONFIG.print_tsr:
+        prs = prs + "\t%f" % (ring / float(sun) * (-1.0))
+
+    if CONFIG.print_angle:
+        prs = prs + "\t%f" % (360 / float(ring+sun))
+
+    print(prs)
+
+def print_examples():
+    print '''
+    1. Check if the following configuration is correct:
+        ring gear   = 36 teeth
+        planet gear = 14 teeth
+        sun gear    = 8 teeth
+
+    > plgcalc.py 36:14:8
+
+    2. Check 10 configurations that are possible for a planetary having:
+        ring gear   = 42 teeth
+        planet gear = 18 teeth
+
+    > plgcalc.py 42:18: -l 10
+
+    3. Same thing for a planetary that has:
+        sun gear    = 12 teeth
+
+    > plgcalc.py ::12 -l 10
+
+    3. Generate all possible configurations for a planetary gear set with following params:
+        sun gear    = 9 teeth
+        planets     = 3 (evenly spaced)
+
+    > plgcalc.py ::8 -l 10 -e -p 3
+    '''
+
+# check command line arguments
+def prepare_arguments():
+    global parser
+
+    parser.add_argument(
+            'gear',
+            metavar='R:P:S',
+            type=str,
+            help='Configuration of planetary gears')
+
+    parser.add_argument(
+            '--examples',
+            dest='actions',
+            action='store_const',
+            const="ex", # dummy
+            help='Print some usage examples as extended help')
+
+    parser.add_argument(
+            '-l','--limit',
+            type=int,
+            default=50,
+            metavar="L",
+            help='Limit of configurations to be calculated (default 50)')
+
+    parser.add_argument(
+            '-p','--planets',
+            type=int,
+            default=0,
+            metavar="P",
+            help='Number of planet gears')
+
+    parser.add_argument(
+            '-e','--even-spacing',
+            dest='options',
+            action='append_const',
+            const=OPTIONS.EVEN_SPACING,
+            help="Show results only for planets evenly spaced")
+
+    parser.add_argument(
+            '-hdr','--header',
+            dest='options',
+            action='append_const',
+            const=OPTIONS.PRINT_HEADER,
+            help="Print configurations header for better description")
+
+    parser.add_argument(
+            '-a','--all',
+            dest='options',
+            action='append_const',
+            const=OPTIONS.PRINT_ALL,
+            help="Print out complete configuration data")
+
+    parser.add_argument(
+            '--tps',
+            dest='options',
+            action='append_const',
+            const=OPTIONS.PRINT_TPS,
+            help="Print planet-sun ratio (ring fixed)")
+
+    parser.add_argument(
+            '--tpr',
+            dest='options',
+            action='append_const',
+            const=OPTIONS.PRINT_TPR,
+            help="Print planet-ring ratio (sun fixed)")
+
+    parser.add_argument(
+            '--tsr',
+            dest='options',
+            action='append_const',
+            const=OPTIONS.PRINT_TSR,
+            help="Print sun-ring ratio (planets fixed)")
+
+    parser.add_argument(
+            '--ang',
+            dest='options',
+            action='append_const',
+            const=OPTIONS.PRINT_ANG,
+            help="Print sun-planets angle")
+
+def parse_input_params():
+
+    global args, parser, ring, planet, sun
+
+    # parse arguments
+    prepare_arguments()
+    args = parser.parse_args()
+
+    # validate input
+    if args.actions is None: args.actions = []
+    if args.options is None: args.options = []
+
+    if args.limit == 0:
+        warning("Invalid linit of configurations (0)")
+        exit()
+
+    CONFIG.from_options(args.options)
+
+# print usage
+def usage():
+    parser.print_help()
+
+# generate all possible configuration for planetary
+def generate_possible_configurations(ring, planet, sun):
+
+    cnt_defined = 0
+    result = None
+    even_planets = None
+
+    if ring != 0: cnt_defined = cnt_defined + 1
+    if planet != 0: cnt_defined = cnt_defined + 1
+    if sun != 0: cnt_defined = cnt_defined + 1
+
+    if args.options is not None and OPTIONS.EVEN_SPACING in args.options:
+        if args.planets == 0:
+            return "define number of planets, or remove even option"
+        even_planets = args.planets
+
+
+    # all set - just validate configuration
+    if cnt_defined == 3:
+        if valid_planetary(ring, planet, sun, even_planets):
+            return [(ring,planet,sun)]
+        else:
+            return "invalid gear configuration"
+
+    # two set - one to calculate
+    if cnt_defined == 2:
+        # calculate ring
+        if ring == 0:
+            ring = 2 * planet + sun
+        # calculate planet
+        elif planet == 0:
+            planet = ring - sun
+            if planet % 2 != 0:
+                return "invalid planet, adjust ring or sun"
+
+            planet = planet/2
+        # calculate sun
+        elif sun == 0:
+            sun = ring - planet * 2
+
+        # done
+        if valid_planetary(ring, planet, sun, even_planets):
+            return [(ring,planet,sun)]
+        else:
+            return "invalid gear configuration"
+
+    # one set - two to generate
+    if cnt_defined == 1:
+
+        result = []
+
+        # RING set => generate possible PLANET and SUN
+        if ring != 0:
+
+            # check even spacing option
+            if even_planets is not None:
+                if ring % even_planets != 0:
+                    return "invalid ring configuration, planets will never be evenly spaced"
+
+            limit = args.limit
+
+            for p in xrange(2,ring/2):
+                if valid_planetary(ring,p,ring - 2 * p,even_planets):
+                    if limit : limit = limit - 1
+                    result.append((ring,p,ring - 2 * p) )
+                    if limit == 0 : break
+
+        # SUN set :) => generate possible RING and PLANET
+        # need additional turn ration TRP (RING/PLANET) set
+        if sun != 0:
+            if args.limit == 0:
+                return "limit must be set, check help"
+
+            # check even spacing option
+            if even_planets is not None:
+                if sun % even_planets != 0:
+                    return "invalid sun configuration, planets will never be evenly spaced"
+
+            p = 1
+            limit = args.limit
+
+            while( True ):
+                if valid_planetary(sun + p * 2, p, sun, even_planets):
+                    result.append((sun + p * 2, p, sun))
+                    limit = limit - 1
+
+                # decrement limit
+                if limit == 0: break
+                p = p + 1
+
+        if planet != 0:
+            if args.limit == 0:
+                return "limit must be set, check help"
+
+            # check even spacing option
+            if even_planets is not None:
+                if planet % even_planets != 0:
+                    return "invalid planet configuration, planets will never be evenly spaced"
+
+            s = 1
+            limit = args.limit
+
+            while( True ):
+                if valid_planetary(planet*2+s,planet,s,even_planets):
+                    result.append((planet*2+s,planet,s))
+                    limit = limit - 1
+
+                # break on limit
+                if limit == 0: break
+                s = s + 1
+
+    return result
+
+# EXAMPLES
+if "--examples" in sys.argv:
+    print_examples()
+    exit()
+
+# PARAMETERS
+parse_input_params()
+
+# CONFIGS
+results = generate_possible_configurations(CONFIG.ring,CONFIG.planet,CONFIG.sun)
+
+# printout error
+if results == None:
+    warning("No results possible")
+elif type(results) == str:
+    warning("No results possible: %s" % results)
+else:
+
+    # print header
+    print_header()
+
+    # results
+    for res in results:
+        print_entry(res[0],res[1],res[2])
+
